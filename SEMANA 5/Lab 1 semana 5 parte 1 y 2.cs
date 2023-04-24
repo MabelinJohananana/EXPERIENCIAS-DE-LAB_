a58b9ad911e4aeadae3085e892fde17c14da39a3
MabelinJohananana <123261466+MabelinJohananana@users.noreply.github.com>
2023-04-23 22:36:19 -0600

    Add files via upload

---
+++ SEMANA 5/Lab 1 semana 5 parte 1 y 2.cs
@@ -0,0 +1,18 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        console WriteLine("Mi segundo programa");
+        Console ReadKey();
+
+        console WriteLine("Mi segundo programa");
+        string Nombre = Console ReadLine();
+        Console WriteLine("Ingrese su edad");
+        string Edad = Console ReadLine();
+        Console WriteLine("Ingrese su carrera");
+        string Carrera = Console ReadLine();
+        Console WriteLine("Carrera su carné");
+        string Carné = Console ReadLine();
+
+        Console.Write("Soy" + Nombre + "tengo" + Edad + "años de estudio la carrera de" + Carrera + "mi número de carné es:" + Carné);
+}          
